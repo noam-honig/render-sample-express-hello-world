9985805ca2d5b1c9b18e292421a2f7479f79ae84
Noam Honig <noam.honig@gmail.com>
2022-09-11 08:42:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) =>{
+  console.log("recieved request");
+  res.type('html').send(html);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
